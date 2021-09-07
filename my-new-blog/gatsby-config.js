72693bf4b22e4d5a5ddc329a120a96169d0e4af2
Marcelo de León <mdeleoncosentino@gmail.com>
2021-09-07 19:26:40 -0300

    Gatsby initial config

---
+++ my-new-blog/gatsby-config.js
@@ -0,0 +1,14 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: 'Portafolio de ML',
+    description: 'Trabajo realizado por Marcelo de León'  
+  },
+  plugins: [],
+}
